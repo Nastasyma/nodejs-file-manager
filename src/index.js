8be5fdf83a96058f326218f0fd3fb9f9da90d0e2
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-02-01 14:43:19 +0300

    feat: add welcome, goodbye messages, add username

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello, user!");
+import { createInterface } from 'node:readline';
+import { stdin as input, stdout as output } from 'process';
+import { displayCurDir } from './utils/curDir.js';
+import { chdir } from 'node:process';
+import { homedir } from 'node:os';
+import { goodbyeMessage } from './utils/goodByeMsg.js';
+
+const usernameArg = process.argv.find(arg => arg.startsWith('--username='));
+const username = usernameArg ? usernameArg.split('=')[1] : 'Guest';
+console.log(`Welcome to the File Manager, ${username}!`);
+
+chdir(homedir());
+
+const rl = createInterface({ input, output });
+
+process.on('SIGINT', () => {
+  goodbyeMessage(username);
+  process.exit();
+});
+
+rl.prompt();
+
+rl.on('line', (input) => {
+  if (input === '.exit') {
+    goodbyeMessage(username);
+    process.exit(0);
+  } else {
+    displayCurDir();
+    rl.prompt();
+  }
+}).on('close', () => {
+  goodbyeMessage(username);
+  process.exit(0);
+});
---
+++ src/utils/curDir.js
@@ -0,0 +1,3 @@
+export const displayCurDir = () => {
+  console.log(`You are currently in ${process.cwd()}`);
+};
