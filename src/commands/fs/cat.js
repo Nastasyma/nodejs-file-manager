0eaad52b18692d9505653ebc7151b04626db417c
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-02-05 15:03:19 +0300

    refactor: edit console log in cat.js

--- src/commands/fs/cat.js
+++ src/commands/fs/cat.js
@@ -1,15 +1,16 @@
 import fs from "fs";
 import { resolve } from "node:path";
+import os from "os";
 import { cwd } from "process";
-import { displayErrorMessage } from "../../utils/messages.js";
 import { log } from "../../utils/coloredMsg.js";
+import { displayErrorMessage } from "../../utils/messages.js";
 
 export const cat = async (path) => {
   const filePath = resolve(cwd(), path);
   const rs = fs.createReadStream(filePath, { encoding: "utf8" });
 
   rs.on("data", (chunk) => {
-    process.stdout.write(chunk);
+    process.stdout.write(chunk + os.EOL);
   });
 
   rs.on("error", (error) => {
